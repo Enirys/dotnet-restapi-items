e8df46839ff8b979572646aabaec410c76f64a6b
Syrine Khelifi <33732676+Enirys@users.noreply.github.com>
2022-01-19 00:51:32 +0100

    Created a new webapi project

---
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository : IItemsRepository
+    {
+        private readonly List<Item> _items = new()
+        {
+            new Item {Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow},
+            new Item {Id = Guid.NewGuid(), Name = "Iron Sword", Price = 25, CreatedDate = DateTimeOffset.UtcNow},
+            new Item {Id = Guid.NewGuid(), Name = "Wooden Shield", Price = 15, CreatedDate = DateTimeOffset.UtcNow},
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return _items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return _items.Where(item => item.Id == id).SingleOrDefault();
+        }
+
+        public void CreateItem(Item item)
+        {
+            _items.Add(item);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var index = _items.FindIndex(existingItem => existingItem.Id == item.Id);
+            _items[index] = item;
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            var index = _items.FindIndex(existingItem => existingItem.Id == id);
+            _items.RemoveAt(index);
+        }
+    }
+}
+
